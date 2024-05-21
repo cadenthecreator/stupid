2023486482ef0ad080359e7b89c915a4014120a8
Caden Ream <114967401+cadenthecreator@users.noreply.github.com>
2024-05-21 15:00:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-print(components.iframe("https://stupidwebsite.streamlit.app/?embed=true:", height=500))
+print(components.iframe("https://stupidwebsite.streamlit.app/?embed=true:"))
